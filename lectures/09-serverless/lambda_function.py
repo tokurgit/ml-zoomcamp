7f34b08034cd09c44287e047036b02f2cf42ea76
Toms Kursits <toms.kursits@gmail.com>
2023-11-27 19:21:19 +0200

    Fix issues with lambda funciton and convert numpy float32 to python float with to_list

--- lectures/09-serverless/lambda_function.py
+++ lectures/09-serverless/lambda_function.py
@@ -3,16 +3,23 @@
 
 import tflite_runtime.interpreter as tflite
 from keras_image_helper import create_preprocessor
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logger.info("Initialize interpreter")
 
 interpreter = tflite.Interpreter(model_path="clothing-model.tflite")
 interpreter.allocate_tensors()
+logger.info("Successfully inititalized interpreter")
 
 input_index = interpreter.get_input_details()[0]["index"]
 output_index = interpreter.get_output_details()[0]["index"]
 
-
+logger.info("Initialize preprocessor")
 preprocessor = create_preprocessor("xception", target_size=(299, 299))
+logger.info("Successfully inititalized preprocessor")
 
 # url = "http://bit.ly/mlbookcamp-pants"
 # X = preprocessor.from_url(url)
@@ -36,15 +43,24 @@ classes = [
 ]
 
 def predict(url):
+    logger.info("Getting data from URL")
     X = preprocessor.from_url(url)
 
+    logger.info("Setting tensors")
     interpreter.set_tensor(input_index, X)
     interpreter.invoke()
+
+    logger.info("Inference...")
     preds = interpreter.get_tensor(output_index)
 
-    return dict(zip(classes, preds[0]))
+    float_predictions = preds[0].tolist()
+    return dict(zip(classes, float_predictions))
 
-def lambda_handle(event, context):
-    url = event["url"]
-    result = predict(url)
-    return result
+def lambda_handler(event, context):
+    logger.info(f"Lambda handler calles with {event=}")
+    try:
+        url = event["url"]
+        result = predict(url)
+        return result
+    except Exception as exc:
+        logger.error(f"Exception {exc} occured during prediction", exc_info=True)
